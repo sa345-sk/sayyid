fe0029e76274c3f6eced69b7a9ac6d1b4f65db38
xxxx <asas>
2023-05-19 18:28:09 +0100

    aliyu

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return ( <nav className="nav">
+        <h1>Code Bean</h1>
+        <div className="links">
+            <a href="/">Home</a>
+            <a href="/contacts">Contacts</a>
+            <a href="/create">New Blog</a>
+        </div>
+    </nav> );
+}
+ 
+export default Navbar;
